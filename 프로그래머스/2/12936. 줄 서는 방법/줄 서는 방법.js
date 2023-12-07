99f8d7a027d4a4abf22d7708dcd8a8beb1251182
Jeong Hun <117993640+wlals7565@users.noreply.github.com>
2023-12-07 16:30:56 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.17 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,17 +1,31 @@
 function solution(n, k) {
-  const factorial = [1];
-  const answer = [];
-  // 팩토리얼 계산
-  for (let i = 1; i <= n; i++) {
-    factorial[i] = factorial[i - 1] * i;
+  let answer = []; // 답안
+  let arr= []; // 미리 저장해둔 크기
+  let sum = 1; // 미리 저장해둔 크기 만들기 용
+  let set = new Set(); // 중복 검사용. 이거 사이즈 배열형태로 빼고 넣기 해야한다. 10개 넣고 시작해야한다.
+  for(let i=0; i<n; i++){
+    set.add(i+1);
   }
-  // 순열 구하기
-  const nums = Array.from({ length: n }, (_, index) => index + 1);
-  for (let i = n - 1; i >= 0; i--) {
-    const index = Math.floor((k - 1) / factorial[i]);
-    answer.push(nums[index]);
-    nums.splice(index, 1);
-    k = ((k - 1) % factorial[i]) + 1;
+  let pick = 1;
+  for(let i=1; i<=n; i++){
+    sum *= i;
+    arr.push(sum);
   }
+  //여기 조건문 잘 봐야한다.
+  while(k>1){
+    for(let i=n-2; i>=0; i--){
+      pick = 1;
+      while(k>arr[i]){
+        k-= arr[i];
+        pick++;
+      }
+      answer.push(Array.from(set.values())[pick-1]); //이거 작동안한다.
+      set.delete(Array.from(set.values())[pick-1]); //이거 작동안한다.
+    }
+  }
+  while(Array.from(set.values())[0]){
+    answer.push(Array.from(set.values())[0]);
+    set.delete(Array.from(set.values())[0]);
+  };
   return answer;
 }
