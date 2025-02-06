3e1ab32f93aa9fe55ce759d7d2bba09550ca03d3
Jeong Hun <117993640+wlals7565@users.noreply.github.com>
2025-02-06 14:29:05 +0900

    [Bronze II] Title: 숫자, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+
+int main() {
+	long long A, B;
+	std::cin >> A >> B;
+	if (A == B) {
+		std::cout << 0 << "\n";
+		return 0;
+	}
+	if (A > B) {
+		int temp = A;
+		A = B;
+		B = temp;
+	}
+	std::cout << B - A - 1 << "\n";
+	for (long long i = A + 1; i < B; i++) {
+		std::cout << i << " ";
+	}
+	return 0;
+}
