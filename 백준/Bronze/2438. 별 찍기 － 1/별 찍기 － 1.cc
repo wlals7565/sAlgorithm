c93a4f684f35d048d399fc8d2781614b738e6095
Jeong Hun <117993640+wlals7565@users.noreply.github.com>
2025-02-07 13:12:32 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int count;
+	std::cin >> count;
+	for (int i = 0; i < count; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+}
