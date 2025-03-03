0269ab218db8be9f7ad9426de3ee8833d3c554d5
Jeong Hun <117993640+wlals7565@users.noreply.github.com>
2025-03-03 10:00:17 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -3,46 +3,55 @@
 #include <string>
 
 int main() {
-    using namespace std;
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);  // cin과 cout 동기화 해제 (필수는 아님)
-
-    string str;
-    stack<char> S;
-
-    while (true) {
-        getline(cin, str);
-        if (str == ".") break;  // 입력이 "."이면 종료
-
-        bool isBalanced = true; // 균형 여부 플래그
-        for (char ch : str) {
-            if (ch == '(' || ch == '[') {
-                S.push(ch);
-            } 
-            else if (ch == ')') {
-                if (!S.empty() && S.top() == '(') {
-                    S.pop();
-                } else {
-                    isBalanced = false;
-                    break; // 잘못된 경우 즉시 종료
-                }
-            } 
-            else if (ch == ']') {
-                if (!S.empty() && S.top() == '[') {
-                    S.pop();
-                } else {
-                    isBalanced = false;
-                    break;
-                }
-            }
-        }
-
-        if (!S.empty()) isBalanced = false;  // 스택에 남은 괄호가 있으면 불균형
-
-        cout << (isBalanced ? "yes\n" : "no\n");
-
-        while (!S.empty()) S.pop();  // 스택 비우기
-    }
-
-    return 0;
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	string str;
+	stack<char> S;
+	getline(cin, str);
+	while (str[0] != '.') {
+		for (int i = 0; i < str.size(); i++) {
+			char ch = str[i];
+			if (ch == '(' || ch == '[') {
+				S.push(ch);
+			}
+			else if (ch == ')') {
+				if (S.empty()) {
+					S.push('n');
+					break;
+				}
+				else if (S.top() == '(') {
+					S.pop();
+				}
+				else if (S.top() != '(') {
+					S.push('n');
+					break;
+				}
+			}
+			else if (ch == ']') {
+				if (S.empty()) {
+					S.push('n');
+					break;
+				}
+				else if (S.top() == '[') {
+					S.pop();
+				}
+				else if (S.top() != '[') {
+					S.push('n');
+					break;
+				}
+			}
+		}
+		if (S.empty()) {
+			cout << "yes\n";
+		}
+		else {
+			cout << "no\n";
+		}
+		while (!S.empty()) {
+			S.pop();
+		}
+		getline(cin, str);
+	}
+	return 0;
 }
