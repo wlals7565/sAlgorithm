49c9ccfbef96a7588c8a16a9ebf3cca604749b9a
Jeong Hun <117993640+wlals7565@users.noreply.github.com>
2025-04-24 10:19:11 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	// 겁나 쉽네???
+
+	int R1, S;
+	cin >> R1 >> S;
+	int R2 = 2 * S - R1;
+	cout << R2;
+}
